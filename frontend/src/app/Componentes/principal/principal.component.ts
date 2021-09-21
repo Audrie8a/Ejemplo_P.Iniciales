5c89c4836a4fe40bffe64b53674d10187d92b660
Audrie8a <ann.audrie8a@gmail.com>
2021-09-20 19:08:13 -0600

    Ejemplo base64

---
+++ frontend/src/app/Componentes/principal/principal.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PrincipalService } from 'src/app/Servicios/principal.service';
+import { DomSanitizer } from '@angular/platform-browser'; 
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+
+  dataUser={
+    idUser: Number,
+    nombre: String,
+    usuario: String,
+    correo: String,
+    contra: String,
+    foto: String
+  }
+
+
+  Usuario:string | null="";
+  ImgUrl: string="../../../assets/Img/descarga.png"
+  fileSelected?:Blob;
+
+  constructor(private sant:DomSanitizer,
+    public princialSevice: PrincipalService,
+    public _routre:Router,
+    public route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    let usuario=this.route.snapshot.paramMap.get("id");
+    this.Usuario=usuario;
+    this.cargarDatos();
+    
+  }
+
+  tiles: Tile[] = [
+    
+    {text: 'Bienvenido', cols: 3, rows: 1, color: 'black'},
+  ];
+
+  async cargarDatos(){
+    let aux= await this.princialSevice.CargarDatosUser(this.Usuario);
+    let json=JSON.stringify(aux)
+    let obj= JSON.parse(json)
+    this.ImgUrl=obj.foto;
+    //this.fileSelected=this.dataURItoBlob(obj.foto)
+    //this.ImgUrl=this.sant.bypassSecurityTrustUrl(window.URL.createObjectURL(this.fileSelected)) as string;
+    
+  }
+
+  dataURItoBlob(dataURI: string) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], { type: 'image/png' });    
+    return blob;
+ }
+  
+
+}
